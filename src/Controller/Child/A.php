22440de6b6b8b013213bb8e3dceeddcd46a75091
Artem Lopata <biozshock@gmail.com>
2021-02-10 19:36:18 +0100

    Bug

---
+++ src/Controller/Child/A.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Child;
+
+/**
+ * @extends A<E>
+ */
+class A extends \App\Controller\Parent\A
+{
+
+}
---
+++ src/Controller/Parent/A.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\Parent;
+
+/**
+ * @template TEntityClass
+ */
+class A
+{
+
+}
